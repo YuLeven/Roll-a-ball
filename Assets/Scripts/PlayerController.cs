ce4b8f3df010fe664d72cfd77de2aac1a806c507
Yuri Levenhagen <yurileven@gmail.com>
2017-06-03 15:04:44 -0300

    Added movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private struct Direction {
+        public const string HORIZONTAL = "Horizontal";
+        public const string VERTICAL = "Vertical";
+    }
+
+    private Rigidbody rigidBody;
+
+    public float speed;
+
+    private void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+
+    private void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis (Direction.HORIZONTAL);
+        float moveVertical = Input.GetAxis (Direction.VERTICAL);
+
+        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rigidBody.AddForce (movement * speed);
+    }
+}
